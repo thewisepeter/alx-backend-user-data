878fb0fae6eecdd09d5caeee105e10d00cc682ee
thewisepeter <rempeterwisdom@gmail.com>
2024-01-11 16:15:18 +0300

    solution to filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+    module that handles personal data
+'''
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''
+        function that obfuscates personal information
+
+        Arguments:
+        fields: a list of strings representing all fields to
+        obfuscate
+        redaction: a string representing by what the field will
+        be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is
+        separating all fields in the log line (message)
+
+        Return: String with redacted values
+    '''
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
