af00e07b24e1b089b34be1d5717c771465b6ddb4
thewisepeter <rempeterwisdom@gmail.com>
2024-01-16 12:14:20 +0300

    implemented basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+    module defining the basic_auth class
+'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+        definition of the class basic-auth
+    '''
+    pass
