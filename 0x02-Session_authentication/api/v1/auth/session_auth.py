1776453051e29b938768b150895c55dffad176b4
thewisepeter <rempeterwisdom@gmail.com>
2024-01-19 11:10:25 +0300

    implemented create_session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,33 @@
     module defining the basic_auth class
 '''
 from api.v1.auth.auth import Auth
+from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''
         definition of the SessionAuth class
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+            instance method def create_session(self, user_id:
+            str = None) -> str: that creates a Session ID for
+            a user_id:
+            Return None if user_id is None
+            Return None if user_id is not a string
+            Otherwise:
+            Generate a Session ID using uuid module and uuid4()
+            like id in Base
+            Use this Session ID as key of the dictionary
+            user_id_by_session_id - the value for this key must be user_id
+            Return the Session ID
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
