0ed28ebf8f19185c52b61b0d08a6e7615eaac824
thewisepeter <rempeterwisdom@gmail.com>
2024-01-19 10:52:43 +0300

    created SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+    module defining the basic_auth class
+'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''
+        definition of the SessionAuth class
+    '''
+    pass
