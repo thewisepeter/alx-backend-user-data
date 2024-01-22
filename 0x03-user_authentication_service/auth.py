a9de5a45ad86272f49d5e17d43cd814027d3fa3e
thewisepeter <rempeterwisdom@gmail.com>
2024-01-22 16:03:20 +0300

    Implementation of auth.py: implemented auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
     arguments and returns bytes.
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -18,3 +21,25 @@ def _hash_password(password: str) -> str:
 
     # Return the hashed password as a byte representation
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''
+            function that registers new user
+        '''
+        try:
+            # find the user with the given email
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            # add user to database
+            return self._db.add_user(email, _hash_password(password))
+
+        else:
+            # if user already exists, throw error
+            raise ValueError('User {} already exists'.format(email))
