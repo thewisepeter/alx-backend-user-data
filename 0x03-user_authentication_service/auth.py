5872ecc369fc94ee9dbc814b996e89c981e13196
thewisepeter <rempeterwisdom@gmail.com>
2024-01-23 12:28:32 +0300

    Implementation of auth.py: implemented get user from session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,12 @@ class Auth:
 
         except NoResultFound:
             return False
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        '''
+            method that finds user by session id
+        '''
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
