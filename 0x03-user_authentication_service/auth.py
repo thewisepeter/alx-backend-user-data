601eb78daaa264079c15ed0a656dd3c0768b3e0a
thewisepeter <rempeterwisdom@gmail.com>
2024-01-23 11:13:12 +0300

    Implementation of auth.py: implemented createsession

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,15 @@ class Auth:
             return False
 
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        '''
+            function that creates a session
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+            return user.session_id
+
+        except NoResultFound:
+            return False
