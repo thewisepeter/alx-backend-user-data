48e5c6a3f47c3f10cd835b3dd9ceb8f19a9a242a
thewisepeter <rempeterwisdom@gmail.com>
2024-01-23 14:04:22 +0300

    Implementation of app.py: implemnted profile function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,5 +64,20 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    '''
+        function that gets user profile
+    '''
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        # Return a JSON response with a 200 status code
+        return jsonify({"email": user.email}), 200
+    else:
+        # all else return a JSON response with a 403 status code
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
