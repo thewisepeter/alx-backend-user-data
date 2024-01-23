ba9f7b5174b19d7dcd1c48cb6b17b0bdde532290
thewisepeter <rempeterwisdom@gmail.com>
2024-01-23 12:49:34 +0300

    Implementation of auth.py: added check for empty session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,6 +80,10 @@ class Auth:
         '''
             method that finds user by session id
         '''
+
+        if session_id is None:
+            return None
+
         try:
             user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
